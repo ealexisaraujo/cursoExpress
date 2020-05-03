fe04c4afa5dcad8df518a940742a967ca6151f1f
Alexis Araujo <ala1289@gmail.com>
2020-05-02 20:11:05 -0500

    Usando Handlebars como template engine

--- ecommerce/index.js
+++ ecommerce/index.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const engines = require('consolidate');
 const productsRouter = require('./routes/products');
+app.engine('hbs', engines.handlebars);
 
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
 
+app.set('view engine', 'hbs');
 app.use('/products', productsRouter);
+
+app.get('/', function (req, res) {
+  res.render('index', { hello: 'hola', world: 'mundo' });
+});
 
 const server = app.listen(8000, function () {
   console.log(`Listening http://localhost:${server.address().port}`);
