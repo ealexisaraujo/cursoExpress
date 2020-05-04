2b274986df50311a58ff148789c9dde4e78a2266
Alexis Araujo <ala1289@gmail.com>
2020-05-04 18:20:10 -0500

    Configuración y uso de Passport.js

--- ecommerce/config/index.js
+++ ecommerce/config/index.js
@@ -11,6 +11,10 @@ const config = {
   dbConnection: process.env.DB_CONNECTION || 'mongodb+srv',
   sentryDns: process.env.SENTRY_DNS,
   sentryId: process.env.SENTRY_ID,
+  authAdminUsername: process.env.AUTH_ADMIN_USERNAME,
+  authAdminPassword: process.env.AUTH_ADMIN_PASSWORD,
+  authAdminEmail: process.env.AUTH_ADMIN_EMAIL,
+  authJwtSecret: process.env.AUTH_JWT_SECRET,
 };
 
 module.exports = { config };
--- ecommerce/index.js
+++ ecommerce/index.js
@@ -3,6 +3,7 @@ const path = require('path');
 const boom = require('@hapi/boom');
 const productsRouter = require('./routes/views/products');
 const productsApiRouter = require('./routes/api/products');
+const authApiRouter = require('./routes/api/auth');
 
 const { config } = require('./config/index');
 
@@ -23,8 +24,10 @@ app.use('/static', express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+// routes
 app.use('/products', productsRouter);
 app.use('/api/products', productsApiRouter);
+app.use('/api/auth', authApiRouter);
 
 // redirect
 app.get('/', function (req, res) {
